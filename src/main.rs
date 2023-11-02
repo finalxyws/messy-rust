ae6a7b897288112097c87ab05a11c157432faf37
finalxyws <finalxyws@gmail.com>
2023-11-02 14:44:02 +0800

    Use ferris_says crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
